e889f141ae2ba4df3a9c49f677eaa99781b54310
Harsh Kumar <maestroharsh@gmail.com>
2022-02-18 23:13:34 -0800

    landing page message updated

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World pushing out to ubuntu"}
+    return {"message": "Hello from the other side!"}
 
 
 
